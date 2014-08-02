c6a3969388a273ccfc8ff8fc669eddd5a45030ed
Aaron Lewis Cunnington <aaron@intellisearch.no>
2014-08-02 10:28:29 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+function jexpr(input, expr) {
+
+	var res = [];
+
+	if(!expr || expr.length == 0) { return res; }
+
+	function walk(obj, step) {
+
+		function addToRes(val) {
+
+			if(step == expr.length - 1) { res.push(val); }
+
+		}
+
+		var step = step || 0;
+
+		if(typeof(expr[step]) === 'object') {
+			// regular expression
+			if(step < expr.length - 1) {
+				// match on keys
+				for(var i in obj) {
+					if(expr[step].test(i)) {
+						addToRes(obj[i]);
+						walk(obj[i], step + 1);
+					}
+				}
+			}
+			else {
+				// match on values
+				if(expr[step].test(obj)) {
+					addToRes(obj);
+					walk(obj[i], step + 1);
+				}
+			}
+		}
+
+		if(typeof(expr[step]) === 'string') {
+
+			if(expr[step] == '*') {
+				// iterate this level
+				for(var i in obj) {
+					addToRes(obj[i]);
+					walk(obj[i], step + 1);
+				}
+			}
+
+			else if(/[0-9]+-[0-9]+/.test(expr[step])) {
+				var range = expr[step].split('-');
+				for(var i = range[0]; i <= range[1]; i++) {
+					addToRes(obj[i]);
+					walk(obj[i], step + 1);
+				}
+			}
+
+		}
+
+		if(typeof(expr[step]) === 'number' || typeof(expr[step]) === 'string') {
+
+			if(obj[expr[step]]) {
+				addToRes(obj[expr[step]]);
+				walk(obj[expr[step]], step + 1);
+			}
+
+		}
+
+	}
+
+	walk(input);
+
+	return res;
+
+}
