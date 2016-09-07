17bbb0e0db09459de56e9d6749a075f2bea4731b
Aaron Cunnington <azcn2503@gmail.com>
2016-09-07 10:11:12 +0100

    Updated to ES6.

--- jexpr.js
+++ jexpr.js
@@ -1,76 +1,81 @@
-function jexpr(input, expr) {
+let jexpr = (input = {}, expr = [], filter = null) => {
 
-	var res = [];
+	let res = [];
 
-	if(!expr || expr.length == 0) { return res; }
+	if (!expr || expr.length == 0) { return res; }
 
-	for(var i in expr) {
-		var re = expr[i].match(/^\/(.+?)\/([igm]*)$/);
-		if(!re) { continue; }
+	for (let i in expr) {
+		if (typeof(expr[i]) === 'number') { continue; }
+		// Match against a regex looking string
+		let re = expr[i].match(/^\/(.+?)\/([igm]*)$/);
+		if (!re) { continue; }
+		// Convert the string to an actual regex with the proper flags (if any)
 		expr[i] = new RegExp(re[1], re[2]);
 	}
 
-	function walk(obj, step) {
+	let walk = (obj, step = 0) => {
 
-		function addToRes(data) {
+		let isValue = () => { return step == expr.length - 1; }
+		let add = (data) => {
+			if (step == expr.length - 1) { res.push(data); return true; }
+			return false;
+		};
 
-			if(step == expr.length - 1) { res.push(data); }
+		// If the expression step is a regular expression
+		if (typeof(expr[step]) === 'object') {
 
-		}
-
-		var step = step || 0;
-
-		val = step == expr.length - 1 ? true : false;
-
-		if(typeof(expr[step]) === 'object') {
-			// regular expression
-			if(val) {
-				// match on values
-				if(expr[step].test(obj)) {
-					addToRes(obj);
-					walk(obj[i], step + 1);
+			// If the expression step is a value and the object tests true against the current expression, add the values if any
+			if (isValue()) {
+				if (expr[step].test(obj)) {
+					add(obj);
 				}
 			}
-			else {
-				// match on keys
-				for(var i in obj) {
-					if(expr[step].test(i)) {
-						addToRes(obj[i]);
+
+			// If obj is an object, iterate over it and test that any of its keys test true against the expression, if they do then add their values if any and walk it
+			if (typeof(obj) === 'object') {
+				for (let i in obj) {
+					if (expr[step].test(i)) {
+						add(obj[i]);
 						walk(obj[i], step + 1);
 					}
 				}
 			}
+
 		}
 
-		if(typeof(expr[step]) === 'string' || typeof(expr[step]) === 'number') {
+		// If the expression step is not a regular expression
+		else {
 
+			// If "*" was passed and obj is an object, iterate over it, add the values if any, and walk it
 			if(expr[step] == '*' && typeof(obj) === 'object') {
-				// iterate this level
-				for(var i in obj) {
-					addToRes(obj[i]);
+				for (let i in obj) {
+					add(obj[i]);
 					walk(obj[i], step + 1);
 				}
 			}
 
-			if(val) {
-
-				if(obj[expr[step]]) {
-					addToRes(obj[expr[step]]);
+			// If the expression step is a value and it represents an existing property in the object, add the values if any, and walk it
+			if (isValue()) {
+				if (obj.hasOwnProperty(expr[step])) {
+					add(obj[expr[step]]);
 					walk(obj[expr[step]], step + 1);
 				}
-
 			}
 
 		}
 
-	}
+	};
 
 	walk(input);
 
+	if (filter != null && !isNaN(filter)) {
+		res = res[filter];
+	}
+
 	return res;
 
-}
+};
 
-if(module && module.exports) {
+if (module && module.exports) {
 	module.exports = jexpr;
 }
